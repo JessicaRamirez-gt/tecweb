979510783d8cdbd243257825f0a6a022217e8cc5
JessicaRamirez-gt <aurorajess0214@gmail.com>
2024-09-04 21:53:39 -0600

    Punto 1

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
 “http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
 <html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title> Practica 5 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo '---PARTE 1<br>';
+            echo '$_myvar es una variable admitida en un archivo php porque inicia con $ <br>';
+            echo '$_7var también es una variable admitida porque empieza en $ y tiene guión bajo<br>';
+            echo 'myvar no es una variable admitida porque no empieza en $ <br>';
+            echo '$myvar es una variable admitida en un archivo php porque inicia con $ <br>';
+            echo '$var7 también es una variable admitida porque empieza en $<br>';
+            echo '$_element es una variable admitida porque empieza en $ y continúa con guión bajo<br>';
+            echo '$house*5 no es una variable válida porque tiene un *, a pesar de iniciar con $<br>';
+        ?>
     
+    </body>
 
 
 </html>
