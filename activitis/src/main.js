f33072338217e272c0eeefe37c62f43b2bc6f7a1
JessicaRamirez-gt <aurorajess0214@gmail.com>
2024-09-26 14:42:10 -0600

    Ejercicio de ejemplo, 1 y avance del 2 :)

---
+++ activitis/src/main.js
@@ -0,0 +1,35 @@
+//Función de ejemplo
+function getDatos(){
+    var nombre = window.prompt("Nombre: ","");
+    var edad= prompt("Edad", "");
+
+    var div1= document.getElementById('nombre');
+    div1.innerHTML ='<h3> Nombre: '+nombre+'</h3>';
+
+
+    var div2= document.getElementById('edad');
+    div2.innerHTML ='<h3> Edad: '+edad+'</h3>';
+}
+
+//Función de Hola Mundo
+function holaMundo(){
+    var mostrar = document.getElementById('Hola');
+    //mostrar = document.write('Hola Mundo');
+    mostrar.innerHTML = '<h3> Hola mundo'
+}
+
+//Función de variables
+function varUno(){
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+
+    document.write( nombre );
+    document.write( '<br>' );
+    document.write( edad );
+    document.write( '<br>' );
+    document.write( altura );
+    document.write( '<br>' );
+    document.write( casado );
+}
